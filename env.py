17f0ceea29a4743f01ca3acf32de252bb7405602
Dbyu85 <lim658589@gmail.com>
2020-12-29 18:17:26 +0000

    fixing bug on login, register and yamii.py, login bug with error 500, register bug error 500, adding functionality to login file

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "Vu1sbmA1m`@OE#MKIXh(tdN/Y;oin7")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://Dbyu85:Mongo0000TheLifeChange@myfirstcluster.ssyob.mongodb.net/yamiiHomeCooking?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "yamiiHomeCooking")
--- yamii.py
+++ yamii.py
@@ -1,40 +1,24 @@
 import pymongo
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
 
 app = Flask(__name__)
 
-MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "yamiiHomeCooking"
-COLLECTION_NAME = "category"
 
-# " collection_name = recipes, galleria"
+# env connection
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
-# Mongo DB connection function.
-
-
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("mongo")
-        return conn
-    except pymongo.error.Connectionfailure as e:
-        print('error: %s') % e
-
-
-conn = mongo_connect(MONGODB_URI)
-
-coll = conn[DBS_NAME][COLLECTION_NAME]
-
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
 
 # default route. 
 
@@ -43,13 +27,17 @@ for doc in documents:
 def index():
     return render_template('index.html')
 
+
 @app.route('/recipes')
 def recipes():
+    recipes = mongo.db.recipes.find()
     return render_template('recipes.html', recipes=mongo.db.recipes.find())
+
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('add_recipe.html', recipes=mongo.db.recipes.find(), category=mongo.db.category.find())
+
 
 @app.route('/load_image')
 def load_image():
@@ -58,9 +46,11 @@ def load_image():
         mongo.save_file(recipe_image.filename, recipe_image)
     return render_template('add_recipe.html', recipe=mongo.db.recipes.find())
 
+
 @app.route('/image/<recipe_image>')
 def image():
     return mongo.send_file(recipe_image)
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -68,12 +58,14 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('recipes'))
 
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     _recipe = mongo.db.recipes.find_one({'_id':ObjectId(recipe_id)})
     _category = mongo.db.category.find()
     category_list = [category for category in _category]
-    return render_template('edit_recipe.html', recipe =_recipe,category = category_list)
+    return render_template('edit_recipe.html', recipe =_recipe, category=category_list)
+
 
 @app.route('/update_recipe/<recipe_id>', methods = ['POST'])
 def update_recipe(recipe_id):
@@ -86,22 +78,66 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('recipes'))
 
+
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
     return redirect(url_for('recipes'))
     
+
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
+
 
 @app.route('/shopping_cart')
 def shopping_cart():
     return render_template('shopping_cart.html')
 
+
 @app.route('/subscription')
 def subscription():
     return render_template('subscription.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # existing users DB
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # new user 'session' cookie
+        session["user"] = request.form.get("username").lower()
+    return render_template('register.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user exist in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password match
+            if check_password_hash(existing_user["password"], request.form.get      ("password")):
+                    session["user"] = request.form.get("username").lower()
+            else:
+                return redirect(url_for("login"))
+
+        else:
+            return redirect(url_for("login"))
+
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
